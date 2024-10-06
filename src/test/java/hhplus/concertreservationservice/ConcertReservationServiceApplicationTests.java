592843663bc007e9a18668ad88d3b75a85eed307
wn1331 <wn1331@gmail.com>
2024-10-06 22:32:27 +0900

    feat : init commit

---
+++ src/test/java/hhplus/concertreservationservice/ConcertReservationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hhplus.concertreservationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertReservationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
