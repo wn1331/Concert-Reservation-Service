b081858e4e608d464a2db5858980e6c13abf807a
wn1331 <wn1331@gmail.com>
2024-11-05 20:57:45 +0900

    feat : [STEP13] infra 패키지 구조 변경

--- src/main/java/hhplus/concertreservationservice/infra/config/JpaConfig.java
+++ src/main/java/hhplus/concertreservationservice/infra/config/JpaConfig.java
@@ -1,4 +1,4 @@
-package hhplus.concertreservationservice.infra.persistence.config;
+package hhplus.concertreservationservice.infra.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
--- src/main/java/hhplus/concertreservationservice/infra/config/RedisConfig.java
+++ src/main/java/hhplus/concertreservationservice/infra/config/RedisConfig.java
@@ -1,9 +1,8 @@
-package hhplus.concertreservationservice.global.config;
+package hhplus.concertreservationservice.infra.config;
 
 import java.time.Duration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
