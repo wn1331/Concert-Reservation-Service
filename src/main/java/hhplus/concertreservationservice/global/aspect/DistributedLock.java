074bf39f0a7ff24cdee3e422fc7d1951166164ce
wn1331 <wn1331@gmail.com>
2024-11-07 12:00:58 +0900

    feat : [STEP14] infra 계층을 제외한 layer(presentation, application, domain) 에서 Redis의 존재를 몰라야 하기 때문에 AOP의 네이밍 변경

--- src/main/java/hhplus/concertreservationservice/global/aspect/DistributedLock.java
+++ src/main/java/hhplus/concertreservationservice/global/aspect/DistributedLock.java
@@ -10,7 +10,7 @@ import java.util.concurrent.TimeUnit;
 @Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-public @interface RedissionPubSubLock {
+public @interface DistributedLock {
     String value(); // Lock의 이름 (고유값)
     long waitTime() default 5L; // Lock획득을 시도하는 최대 시간 (ms)
     long leaseTime() default 2L; // 락을 획득한 후, 점유하는 최대 시간 (ms)
--- src/main/java/hhplus/concertreservationservice/global/aspect/RedissonDistributedLockAspect.java
+++ src/main/java/hhplus/concertreservationservice/global/aspect/RedissonDistributedLockAspect.java
@@ -17,15 +17,15 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 @RequiredArgsConstructor
-public class RedissonPubSubLockAspect {
+public class RedissonDistributedLockAspect {
 
     private final RedissonClient redissonClient;
     private static final String TOPIC_NAME = "lockTopic";  // 락 상태를 전달하는 Pub/Sub 토픽 이름
 
-    @Around("@annotation(redissionPubSubLock)")
-    public Object handleRedissionPubSubLock(ProceedingJoinPoint joinPoint, RedissionPubSubLock redissionPubSubLock) throws Throwable {
+    @Around("@annotation(distributedLock)")
+    public Object handleRedissionPubSubLock(ProceedingJoinPoint joinPoint, DistributedLock distributedLock) throws Throwable {
         // 락 키 생성 및 검증
-        String lockKey = CustomSpringELParser.parseKey(joinPoint, redissionPubSubLock.value());
+        String lockKey = CustomSpringELParser.parseKey(joinPoint, distributedLock.value());
         if (lockKey == null) {
             throw new CustomGlobalException(ErrorCode.PUBSUB_LOCK_KEY_NOT_NULL);  // 락 키가 없을 경우 예외 발생
         }
@@ -35,7 +35,7 @@ public class RedissonPubSubLockAspect {
 
         try {
             // 락 획득 시도: 지정된 시간 동안 락을 시도하고, 실패 시 경고 및 예외 처리
-            boolean isLocked = lock.tryLock(redissionPubSubLock.waitTime(), redissionPubSubLock.leaseTime(), redissionPubSubLock.timeUnit());
+            boolean isLocked = lock.tryLock(distributedLock.waitTime(), distributedLock.leaseTime(), distributedLock.timeUnit());
             if (!isLocked) {
                 log.warn("Failed to acquire lock: {}", lockKey);
                 throw new CustomGlobalException(ErrorCode.CANNOT_ACQUIRE_LOCK);
