0b0725c883046997fba1d55dd276ccb7b2c2ccd3
wn1331 <wn1331@gmail.com>
2024-11-19 07:41:55 +0900

    feat : [STEP17] Zookeeper 사용 및 카프카 도커이미지 변경, TestContainer 대신 EmbeddedKafka 사용

--- src/test/java/hhplus/concertreservationservice/integration/kafka/KafkaConnectionTest.java
+++ src/test/java/hhplus/concertreservationservice/integration/kafka/KafkaConnectionTest.java
@@ -13,35 +13,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.KafkaContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
-
-
-
-
 
 @SpringBootTest
-@Testcontainers
 @ActiveProfiles("test")
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9093", "port=9093"})
 class KafkaConnectionTest {
 
     private static final Logger log = LoggerFactory.getLogger(KafkaConnectionTest.class);
 
-
-    @Container
-    static final KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.5.0"));
-
-    @DynamicPropertySource
-    static void overrideProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
-    }
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 //    private KafkaTemplate<String, ConcertPayment> kafkaTemplate;
@@ -51,7 +34,7 @@ class KafkaConnectionTest {
 
 
     @Test
-    @DisplayName("[성공] 카프카 연동 테스트, 브로커에 test-topic 토픽으로 test-message를 전송하고 최대 10초 대기하면서 listener가 받는 메시지와 일치한지 확인한다.")
+    @DisplayName("[성공] 카프카 연동 테스트, 브로커에 test-topic 토픽으로 test-message를 전송하고 최대 10초 대기하면서 consumer가 받는 메시지와 일치한지 확인한다.")
     void produceTestMessage() {
         String topic = "test-topic";
         String message = "test-message";
